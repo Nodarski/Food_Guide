a3fc54829ecb4640911a6c1d02736d83d61acf99
AbrahamTesla <manlangitv@gmail.com>
2018-05-26 11:56:50 -0400

    add some changes

---
+++ assets/app.js/app.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAUkKSW7gOvd4aaVFe7YLeYfPo-IgUoAho",
+    authDomain: "food-guide-48390.firebaseapp.com",
+    databaseURL: "https://food-guide-48390.firebaseio.com",
+    projectId: "food-guide-48390",
+    storageBucket: "",
+    messagingSenderId: "121003907285"
+  };
+  firebase.initializeApp(config);
+</script>
