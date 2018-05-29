bdfc7b3910dc0e1cdcbb09dae0a2106a980f3102
Nodarski <nodarlaw@gmail.com>
2018-05-28 21:11:52 -0600

    firebase added to app.js

--- assets/app.js/app.js
+++ assets/app.js/app.js
@@ -1,14 +1,29 @@
+console.log('hello world');
 
-<script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"></script>
-<script>
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAUkKSW7gOvd4aaVFe7YLeYfPo-IgUoAho",
-    authDomain: "food-guide-48390.firebaseapp.com",
-    databaseURL: "https://food-guide-48390.firebaseio.com",
-    projectId: "food-guide-48390",
-    storageBucket: "",
-    messagingSenderId: "121003907285"
-  };
-  firebase.initializeApp(config);
-</script>
+var config = {
+  apiKey: "AIzaSyB7qlfahOZn3WCoXsne0WY26Z-dE2N_m1U",
+  authDomain: "ultimate-food-guide.firebaseapp.com",
+  databaseURL: "https://ultimate-food-guide.firebaseio.com",
+  projectId: "ultimate-food-guide",
+  storageBucket: "",
+  messagingSenderId: "674703006763"
+};
+firebase.initializeApp(config);
+
+  var database = firebase.database();
+  var rootRef = firebase.database().ref();
+
+console.log(rootRef)
+
+
+  // var title = "space+jam";
+  //   var queryURL = "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
+
+  //   $.ajax({
+  //     url: queryURL,
+  //     method: "GET"
+  //   })(function(response) {
+  //     console.log(response);
+  //     console.log(response.Runtime);
+  //   });
+
