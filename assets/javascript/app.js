2727224ac74500a32b1baf02dc08f3afb3079ca9
Morrowii <sadler.m.ryan@gmail.com>
2018-05-29 18:18:19 -0400

    fixed JS link and search bar spacing bug

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,8 @@
-console.log('hello world');
 
-var config = {
-  apiKey: "AIzaSyB7qlfahOZn3WCoXsne0WY26Z-dE2N_m1U",
-  authDomain: "ultimate-food-guide.firebaseapp.com",
-  databaseURL: "https://ultimate-food-guide.firebaseio.com",
-  projectId: "ultimate-food-guide",
-  storageBucket: "",
-  messagingSenderId: "674703006763"
-};
-firebase.initializeApp(config);
-
-  var database = firebase.database();
-  var rootRef = firebase.database().ref();
+var database = firebase.database();
+var rootRef = firebase.database().ref();
 
 console.log(rootRef)
-
 
   // var title = "space+jam";
   //   var queryURL = "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
@@ -26,4 +14,3 @@ console.log(rootRef)
   //     console.log(response);
   //     console.log(response.Runtime);
   //   });
-
